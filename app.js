09718e2c07493bca7acd687062203800ab4d1765
Mia Chen <mia89926@gmail.com>
2020-05-19 20:47:19 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Record = require('./models/record')
 require('./config/mongoose')
 
 const app = express()
@@ -9,7 +10,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    Record.find()
+      .lean()
+      .then(records => res.render('index', { records }))
+      .catch(error => console.log(error))
 })
 
 app.listen(PORT, () => [
