3468470495a26f287a074cfd1030c09e52b6714d
Mia Chen <mia89926@gmail.com>
2020-05-20 18:34:07 +0800

    feat: add totalAmount to index.hbs

--- app.js
+++ app.js
@@ -9,11 +9,26 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// READ view all records
 app.get('/', (req, res) => {
     Record.find()
       .lean()
-      .then(records => res.render('index', { records }))
+      .then((records) => {
+          // 加總所有花費
+          let amounts = []
+          let totalAmount = 0
+          records.forEach(r => amounts.push(r.amount))
+          for (let i in amounts) {
+              totalAmount += amounts[i]
+          }
+          res.render('index', { records, totalAmount })
+      })
       .catch(error => console.log(error))
+})
+
+// CREATE add new record
+app.get('records/new', (req,res) => {
+    return res.render('new')
 })
 
 app.listen(PORT, () => [
